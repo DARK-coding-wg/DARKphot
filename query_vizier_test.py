9cdc17131fe4b5e09e36d7c9db2db5d6c9dab29c
Per Andersen <perandersen@dark-cosmology.dk>
2016-02-29 15:37:18 +0100

    Added tests for _create_url

--- query_vizier_test.py
+++ query_vizier_test.py
@@ -33,4 +33,22 @@ class TestQuery(unittest.TestCase):
         new_frame = self.vizier._replace_frequency_with_wavelength(input_frame)
         wavelengths = pd.DataFrame({'source_id':np.arange(5), 'sed_wave':[2.997925e3, 2.997925e7, np.inf, np.nan, np.nan]})
         pdt.assert_series_equal(wavelengths['sed_wave'], new_frame['sed_wave'])
-
+    
+    
+    """ Test for _create_url """
+    def test_create_url_name_input(self):
+        """ Check _create_url with string name input"""
+        vizier = VizierCatalog()
+        url_return = vizier._create_url('vega')
+        self.assertTrue(url_return=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=vega&-c.rs=1.50")
+    
+    def test_create_url_tuple_input(self):
+        """ Check _create_url with tuple of floats"""
+        vizier = VizierCatalog()
+        url_return = vizier._create_url((279.234733,38.783))
+        self.assertTrue(url_return=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=279.234733+38.783000&-c.rs=1.50")
+    
+    def test_create_url_one_element_list(self):
+        """ Check _create_url with tuple of floats"""
+        vizier = VizierCatalog()
+        self.assertRaises(IOError,vizier._create_url,(279.234733))
