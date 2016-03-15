575d5fc67d35a45600686d68da9fb1a71d97aaee
Peter Laursen <pela@dark-cosmology.dk>
2016-03-15 14:24:04 +0100

    Minor changes

--- query_vizier.py
+++ query_vizier.py
@@ -181,14 +181,19 @@ class VizierCatalog(object):
         return df
 
     def _check_coords(self, position):
-        """ Check that a source position is in the correct format. If not change to default coords.
+        """
+        Check that a source position is in the correct format.
+        If not, change to default coords.
 
         Args:
-            position - source position. Must be 2-element list-like object with position[0] = ra, position[1] = dec.
-                       Acceptable formats for ra & dec are decimal degrees as single floats or ints, or
-                       sexagisimal as 3-element tuple/lists of (h, m, s), or strings with h/m/s
+            position - source position. Must be 2-element list-like object with
+                       position[0] = RA, position[1] = dec.
+                       Acceptable formats for RA & dec are decimal degrees as
+                       single floats or ints, or sexagisimal as 3-element
+                       list-like object of (h,m,s), or strings with h/m/s.
+
         Returns:
-            source_pos - Tuple of (ra,dec) in degrees.
+            source_pos - Tuple of (RA,dec) in degrees.
         """
         in_degrees = isinstance(position[0], (float, int)) & isinstance(position[1], (float, int))
       # pdb.set_trace()
@@ -198,13 +203,19 @@ class VizierCatalog(object):
             try:
                 coords = SkyCoord(position[0], position[1], unit=(u.hourangle, u.deg))
                 new_pos = (coords.ra.deg, coords.dec.deg)
+            except ValueError as exc:
+                print exc
+                raise SystemExit
             except:
-                logging.critical('WARNING: ra and dec format cannot be read')
+                logging.critical('WARNING: RA and dec format cannot be read')
                 raise
         else:
             new_pos = (float(position[0]), float(position[1]))
-        return new_pos
-        
+
+        if not (  0<=new_pos[0]<=360): raise ValueError( 'RA should be in the range [0,360]')
+        if not (-90<=new_pos[1]<=90 ): raise ValueError('dec should be in the range [-90,90]')
+
+        return new_pos       
 
 ####################
 ####################
