fb7b8a91ba109a5c578e63f8fe116c11b2781c39
nylee23 <nylee23@gmail.com>
2016-03-17 14:58:34 +0100

    Syntax changes based on PEP8 guidelines

--- query_vizier.py
+++ query_vizier.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# 
+#
 # Program: query_vizier
 #
 # Author: Nick Lee
@@ -16,27 +16,24 @@
 
 # Import Libraries
 import numpy as np
-import matplotlib.pyplot as pl
 import pandas as pd
-import math
-import pdb
 import os
-import sys
 import argparse
 import warnings
 import requests
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-import pdb
 
-## Debugging module and intital setup
+# Debugging module and intital setup
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
 
-###############
-### Classes ###
-###############
+###########
+# Classes #
+###########
+
+
 class VizierCatalog(object):
     """ Class for creating a photometric catalog from Vizier """
 
@@ -49,15 +46,15 @@ class VizierCatalog(object):
             radius - Defines region to search for counterparts in arcsec. Default is 1.5
         Returns:
             vizier_data - pandas dataframe containing all observations from Vizier and an additional source_id column that identifies which source each obsersvation is for
-            source_info - pandas dataframe containing the names, positions, and source_id of each input source. 
+            source_info - pandas dataframe containing the names, positions, and source_id of each input source.
         """
-        ## Figure out if inputs are names or positions
+        # Figure out if inputs are names or positions
         names = []
         pos = []
         source_id = []
-        
+
         for ii, source in enumerate(source_list):
-            ## Single string = Source name
+            # Single string = Source name
             if isinstance(source, str):
                 names.append(source)
                 pos.append((np.nan, np.nan))
@@ -66,10 +63,10 @@ class VizierCatalog(object):
                 names.append(np.nan)
                 pos.append(self._check_coords(source))
                 source_id.append(ii)
-        
-        ## Create pandas dataframe containing source_id & source name and/or position
-        self.source_info = pd.DataFrame({'names':names, 'positions':pos, 'source_id':source_id, 'input':source_list})
-        ## Go through Vizier queries 
+
+        # Create pandas dataframe containing source_id & source name and/or position
+        self.source_info = pd.DataFrame({'names': names, 'positions': pos, 'source_id': source_id, 'input': source_list})
+        # Go through Vizier queries
         vizier_data = self.get_all_dataframes(source_list, source_id=source_id, radius=radius)
         return vizier_data, self.source_info
 
@@ -103,39 +100,39 @@ class VizierCatalog(object):
             finally:
                 os.remove(vot_name)
         all_frames = self._replace_frequency_with_wavelength(pd.concat(list_of_frames))
-        return all_frames  
+        return all_frames
 
     def _read_vo_table(self, filename):
         """
-        Reads in a VOTable and returns a pandas dataframe containing the data (private).
-    
+        Reads VOTable and returns a pandas dataframe (private).
+
         Args:
             filename - Full path filename of the VOTable. Should have a file extension of .vot
         Returns:
             pandas_table - A pandas dataframe containing the data from a VOTable
         Raises:
-            ValueError: The VOTable exists but is empty. 
+            ValueError: The VOTable exists but is empty.
             IOError: Improper filename provided. Most likely the file does not exist.
         """
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             try:
                 tab = Table.read(filename)
-            except ValueError as exc:
-                raise 
-            except IOError as exc:
+            except ValueError:
+                raise
+            except IOError:
                 logging.critical('Invalid filename passed to read_vo_table')
-                raise 
+                raise
         return tab.to_pandas()
 
     def _download_from_vizier(self, url, filename):
         """ Download the VOTable from Vizier Photometric Table and save to designated filename.
-    
+
         Args:
             url - Full url to Vizier Photometric Table query
             filename - local filename to save the VOTable
         Returns:
-            None 
+            None
         Side Effects:
             Downloads and saves the VOTable found at url to filename.
         """
@@ -145,7 +142,7 @@ class VizierCatalog(object):
             for ii, chunk in enumerate(response.iter_content(chunk_size=1024)):
                 if chunk:                 # filter out keep-alive new chunks
                     vo_table.write(chunk)
-                    #f.flush()
+                    # f.flush()
 
     def _create_url(self, source, radius=1.5):
         """ Make the correct URL for the Vizier query
@@ -154,7 +151,7 @@ class VizierCatalog(object):
         Args:
             source - Either a tuple of (ra, dec) in decimal degrees or a string that represents the name of the source
         Keywords:
-            radius - Search radius in arcsec. Default is 1.5 arcsec. 
+            radius - Search radius in arcsec. Default is 1.5 arcsec.
         Returns:
             url - URL where VOTable from Vizier Photometric Table can be found
         """
@@ -175,7 +172,7 @@ class VizierCatalog(object):
         Treats any negative frequencies as non-detections and replaces them with NaNs """
         df = catalog.copy()
         speed_of_light = 299792.458  # [um * GHz]
-        df['sed_wave'] = speed_of_light/df['sed_freq']  # Converts from GHz to microns
+        df['sed_wave'] = speed_of_light / df['sed_freq']  # Converts from GHz to microns
         df.loc[df['sed_wave'] < 0, 'sed_wave'] = np.nan
         df.drop('sed_freq', axis=1, inplace=True)
         return df
@@ -196,9 +193,8 @@ class VizierCatalog(object):
             source_pos - Tuple of (RA,dec) in degrees.
         """
         in_degrees = isinstance(position[0], (float, int)) & isinstance(position[1], (float, int))
-      # pdb.set_trace()
-        if isinstance(position[0],(np.ndarray, list)):
-             position = (tuple(position[0]), tuple(position[1]))
+        if isinstance(position[0], (np.ndarray, list)):
+            position = (tuple(position[0]), tuple(position[1]))
         if not in_degrees:
             try:
                 coords = SkyCoord(position[0], position[1], unit=(u.hourangle, u.deg))
@@ -212,51 +208,48 @@ class VizierCatalog(object):
         else:
             new_pos = (float(position[0]), float(position[1]))
 
-        if not (  0<=new_pos[0]<=360): raise ValueError( 'RA should be in the range [0,360]')
-        if not (-90<=new_pos[1]<=90 ): raise ValueError('dec should be in the range [-90,90]')
+        if not (0 <= new_pos[0] <= 360):
+            raise ValueError('RA should be in the range [0,360]')
+        if not (-90 <= new_pos[1] <= 90):
+            raise ValueError('dec should be in the range [-90,90]')
 
-        return new_pos       
+        return new_pos
 
-####################
-####################
+
 def parse_args():
     '''
     Read command line arguments
     '''
-    ## Define a parser
+    # Define a parser
     parser = argparse.ArgumentParser(description='Modules to query the Vizier Photometric Catalog')
 
-    ## Add arguments
-    parser.add_argument('-d','--debug',action='store_true',help='Turn on debugging messages')
+    # Add arguments
+    parser.add_argument('-d', '--debug', action='store_true', help='Turn on debugging messages')
 
-    ## Use parser to interpret the command line arguments
+    # Use parser to interpret the command line arguments
     inputs = vars(parser.parse_args())
 
-    ## Set debugging options
-    if inputs['debug']==False:
+    # Set debugging options
+    if inputs['debug'] == False:
         logging.disable(logging.INFO)  # Turns off debug messages
     else:
         logging.disable(logging.NOTSET)
         logging.debug('Start of program')
 
-#####################
-###### Run Code #####
-#####################
+############
+# Run Code #
+############
 if __name__ == '__main__':
-    ## Process command line arguments
+    # Process command line arguments
     parse_args()
 
-    ## A couple sources, including one that doesn't have any counterparts
-    ra_list = [187.27832916, 150.231314, 149.426254, '15 34 57.224'] 
-    dec_list = [2.05199, -4.1234,  2.073906, '+23 30 11.610'] 
-    #ra_list = ['14:23:45.45'] #
-    #dec_list = ['02:10:45.45'] #
+    # A couple sources, including one that doesn't have any counterparts
+    ra_list = [187.27832916, 150.231314, 149.426254, '15 34 57.224']
+    dec_list = [2.05199, -4.1234, 2.073906, '+23 30 11.610']
+    # ra_list = ['14:23:45.45'] #
+    # dec_list = ['02:10:45.45'] #
     name_list = ['vega', 'ic348', 'not_a_source']
-    
+
     vizier = VizierCatalog()
     pos_phot, pos_sources = vizier.query_vizier(zip(ra_list, dec_list))
     named_phot, named_sources = vizier.query_vizier(name_list)
-
-
-
-        
