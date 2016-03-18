f102a588d89aa781872b888af1de566576f50506
Peter Laursen <pela@dark-cosmology.dk>
2016-03-19 00:07:46 +0100

    Started unittesting edge cases for check_coords

--- query_vizier.py
+++ query_vizier.py
@@ -181,30 +181,40 @@ class VizierCatalog(object):
         return df
 
     def _check_coords(self, position):
-        """ Check that a source position is in the correct format. If not change to default coords.
-
+        """
+        Check that a source position is in the correct format.
+        If not, change to default coords.
         Args:
-            position - source position. Must be 2-element list-like object with position[0] = ra, position[1] = dec.
-                       Acceptable formats for ra & dec are decimal degrees as single floats or ints, or
-                       sexagisimal as 3-element tuple/lists of (h, m, s), or strings with h/m/s
+            position - source position. Must be 2-element list-like object with
+                       position[0] = RA, position[1] = dec.
+                       Acceptable formats for RA & dec are decimal degrees as
+                       single floats or ints, or sexagisimal as 3-element
+                       list-like object of (h,m,s), or strings with h/m/s.
         Returns:
-            source_pos - Tuple of (ra,dec) in degrees.
+            source_pos - Tuple of (RA,dec) in degrees.
         """
         in_degrees = isinstance(position[0], (float, int)) & isinstance(position[1], (float, int))
-        
-        if isinstance(position[0],(np.ndarray, list)):
-             position = (tuple(position[0]), tuple(position[1]))
+        if isinstance(position[0], (np.ndarray, list)):
+            position = (tuple(position[0]), tuple(position[1]))
         if not in_degrees:
             try:
                 coords = SkyCoord(position[0], position[1], unit=(u.hourangle, u.deg))
                 new_pos = (coords.ra.deg, coords.dec.deg)
+            except ValueError as exc:
+                print exc
+                raise SystemExit
             except:
-                logging.critical('WARNING: ra and dec format cannot be read')
+                logging.critical('WARNING: RA and dec format cannot be read')
                 raise
         else:
             new_pos = (float(position[0]), float(position[1]))
+
+        if not (0 <= new_pos[0] <= 360):  raise ValueError('RA = {:.2f}, but should be in the range [0,360]'.format(new_pos[0]))
+        if not (-90 <= new_pos[1] <= 90): raise ValueError('dec should be in the range [-90,90]')
+
         return new_pos
-        
+
+
 
 ####################
 ####################
--- query_vizier_test.py
+++ query_vizier_test.py
@@ -31,77 +31,77 @@ class TestQuery(unittest.TestCase):
     def setUp(self):
         self.vizier = VizierCatalog()
 
-    def assert_equal_pos(self, pos1, pos2, delta=None):
-        """ Check two positions and make sure they're the same within a certain distance """
-        if delta is None:
-            delta = 0.01/3600.  # 1/10 arcsec
-        pos1 = self.vizier._check_coords(pos1)
-        pos2 = self.vizier._check_coords(pos2)
-        self.assertAlmostEqual(pos1[0], pos2[0], delta=delta)  
-        self.assertAlmostEqual(pos1[1], pos2[1], delta=delta)  
+    # def assert_equal_pos(self, pos1, pos2, delta=None):
+    #     """ Check two positions and make sure they're the same within a certain distance """
+    #     if delta is None:
+    #         delta = 0.01/3600.  # 1/10 arcsec
+    #     pos1 = self.vizier._check_coords(pos1)
+    #     pos2 = self.vizier._check_coords(pos2)
+    #     self.assertAlmostEqual(pos1[0], pos2[0], delta=delta)  
+    #     self.assertAlmostEqual(pos1[1], pos2[1], delta=delta)  
         
-    ### Test for _replace_frequency_with_wavelength ###
-    def test_freq_to_wave(self):
-        """ Check conversion from frequency in GHz to wavelength in microns """
-        input_frame = pd.DataFrame({'source_id':np.arange(5), 'sed_freq':[100, 0.01, 0, np.nan, -99]})
-        new_frame = self.vizier._replace_frequency_with_wavelength(input_frame)
-        wavelengths = pd.DataFrame({'source_id':np.arange(5), 'sed_wave':[2.997925e3, 2.997925e7, np.inf, np.nan, np.nan]})
-        pdt.assert_series_equal(wavelengths['sed_wave'], new_frame['sed_wave'])
+    # ### Test for _replace_frequency_with_wavelength ###
+    # def test_freq_to_wave(self):
+    #     """ Check conversion from frequency in GHz to wavelength in microns """
+    #     input_frame = pd.DataFrame({'source_id':np.arange(5), 'sed_freq':[100, 0.01, 0, np.nan, -99]})
+    #     new_frame = self.vizier._replace_frequency_with_wavelength(input_frame)
+    #     wavelengths = pd.DataFrame({'source_id':np.arange(5), 'sed_wave':[2.997925e3, 2.997925e7, np.inf, np.nan, np.nan]})
+    #     pdt.assert_series_equal(wavelengths['sed_wave'], new_frame['sed_wave'])
 
-    ## Tests for _read_vo_table
-    def test_read_vega_table(self):
-        """ Make sure the vega table is being read correctly """
-        vega_path = 'test_data/vega_vizier_votable.vot'
-        if not os.path.exists(vega_path):
-            vega_url = 'http://vizier.u-strasbg.fr/viz-bin/sed?-c=Vega&-c.rs=1.5'
-            self.vizier._download_from_vizier(vega_url, vega_path)
-        vega_table = self.vizier._read_vo_table(vega_path)
-        vega_pos = (279.23473333333334, 38.78368888888889)
-        self.assert_equal_pos((vega_table['_RAJ2000'].mean(), vega_table['_DEJ2000'].mean()), vega_pos, delta=1.5/3600.)
-    def test_read_non_existant_file(self):
-        """ Check to make sure the right Exceptions are raised if trying to read a non-existant filename"""
-        bad_path = 'test_data/non_existant_file.vot'
-        if os.path.exists(bad_path):
-            os.remove(bad_path)
-        with self.assertRaises(IOError):
-            self.vizier._read_vo_table(bad_path)
-    def test_empty_file(self):
-        """ Check to make sure the right Exceptions are raised if trying to read a votable with no data """
-        empty_path = 'test_data/empty_votable.vot'
-        if not os.path.exists(empty_path):
-            empty_url = 'http://vizier.u-strasbg.fr/viz-bin/sed?-c=not_a_source&-c.rs=1.5'
-            self.vizier._download_from_vizier(empty_url, empty_path)
-        with self.assertRaises(ValueError):
-            empty_table = self.vizier._read_vo_table(empty_path)
+    # ## Tests for _read_vo_table
+    # def test_read_vega_table(self):
+    #     """ Make sure the vega table is being read correctly """
+    #     vega_path = 'test_data/vega_vizier_votable.vot'
+    #     if not os.path.exists(vega_path):
+    #         vega_url = 'http://vizier.u-strasbg.fr/viz-bin/sed?-c=Vega&-c.rs=1.5'
+    #         self.vizier._download_from_vizier(vega_url, vega_path)
+    #     vega_table = self.vizier._read_vo_table(vega_path)
+    #     vega_pos = (279.23473333333334, 38.78368888888889)
+    #     self.assert_equal_pos((vega_table['_RAJ2000'].mean(), vega_table['_DEJ2000'].mean()), vega_pos, delta=1.5/3600.)
+    # def test_read_non_existant_file(self):
+    #     """ Check to make sure the right Exceptions are raised if trying to read a non-existant filename"""
+    #     bad_path = 'test_data/non_existant_file.vot'
+    #     if os.path.exists(bad_path):
+    #         os.remove(bad_path)
+    #     with self.assertRaises(IOError):
+    #         self.vizier._read_vo_table(bad_path)
+    # def test_empty_file(self):
+    #     """ Check to make sure the right Exceptions are raised if trying to read a votable with no data """
+    #     empty_path = 'test_data/empty_votable.vot'
+    #     if not os.path.exists(empty_path):
+    #         empty_url = 'http://vizier.u-strasbg.fr/viz-bin/sed?-c=not_a_source&-c.rs=1.5'
+    #         self.vizier._download_from_vizier(empty_url, empty_path)
+    #     with self.assertRaises(ValueError):
+    #         empty_table = self.vizier._read_vo_table(empty_path)
         
-    ### Test for _create_url ###
-    def test_create_url_name_input(self):
-        """ Check _create_url with string name input"""
-        url = self.vizier._create_url('vega')
-        self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=vega&-c.rs=1.50")
-    def test_create_url_tuple_input(self):
-        """ Check _create_url with tuple of floats"""
-        url = self.vizier._create_url((279.234733,38.783))
-        self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=279.234733+38.783000&-c.rs=1.50")
-    def test_create_url_zero_input(self):
-        """ Check _create_url with tuple of 0 ints"""
-        url = self.vizier._create_url((0,0))
-        self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=0.000000+0.000000&-c.rs=1.50")
-    def test_create_url_one_element_list(self):
-        """ Check _create_url with tuple of floats"""
-        self.assertRaises(IOError,self.vizier._create_url,(279.234733))
-    def test_create_url_negative_dec(self):
-        """ Check that negative declination is handled correctly """
-        url = self.vizier._create_url((1.0,-38.783),radius=10.)
-        self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=1.000000-38.783000&-c.rs=10.00")
-    def test_create_url_negative_ra(self):
-        """ Check that negative declination is handled correctly """
-        url = self.vizier._create_url((-100.0,-38.783),radius=10.)
-        self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=-100.000000-38.783000&-c.rs=10.00")
+    # ### Test for _create_url ###
+    # def test_create_url_name_input(self):
+    #     """ Check _create_url with string name input"""
+    #     url = self.vizier._create_url('vega')
+    #     self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=vega&-c.rs=1.50")
+    # def test_create_url_tuple_input(self):
+    #     """ Check _create_url with tuple of floats"""
+    #     url = self.vizier._create_url((279.234733,38.783))
+    #     self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=279.234733+38.783000&-c.rs=1.50")
+    # def test_create_url_zero_input(self):
+    #     """ Check _create_url with tuple of 0 ints"""
+    #     url = self.vizier._create_url((0,0))
+    #     self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=0.000000+0.000000&-c.rs=1.50")
+    # def test_create_url_one_element_list(self):
+    #     """ Check _create_url with tuple of floats"""
+    #     self.assertRaises(IOError,self.vizier._create_url,(279.234733))
+    # def test_create_url_negative_dec(self):
+    #     """ Check that negative declination is handled correctly """
+    #     url = self.vizier._create_url((1.0,-38.783),radius=10.)
+    #     self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=1.000000-38.783000&-c.rs=10.00")
+    # def test_create_url_negative_ra(self):
+    #     """ Check that negative declination is handled correctly """
+    #     url = self.vizier._create_url((-100.0,-38.783),radius=10.)
+    #     self.assertTrue(url=="http://vizier.u-strasbg.fr/viz-bin/sed?-c=-100.000000-38.783000&-c.rs=10.00")
         
 
     ### Test for _check_coords ###
-    def test_check_coords_conversion(self):
+    def test_check_coords_formats(self):
         """Check that various representations of Vega coords return the same result"""
         formats = ['tuples',
                     'lists',
@@ -133,3 +133,22 @@ class TestQuery(unittest.TestCase):
             current_result = self.vizier._check_coords((r,d))
             pdt.assert_almost_equal(current_result, result)
 
+    def test_check_coords_edge_cases(self):
+        """Check that unphysical inpiut is handled appropriately"""
+        # RAinput  = [0,np.nan,-1,361,np.inf,1e-30,1e30]
+        # decinput = [0,np.nan,-91,91,np.inf,1e-30,1e30]
+        # RAokay   = 30.
+        # decokay  = 30.
+        # for r,d in zip(RAinput,decinput):
+        #     try:
+        #         current_result = self.vizier._check_coords((r,decokay))
+        #         print r,current_result
+        #     except:
+        #         print r, 'failed'
+
+        #     try:
+        #         current_result_dec = self.vizier._check_coords((d,raokay))
+        #         print d, current_result
+        #     except:
+        #         print d, 'failed'
+        # with self.assertRaises(ValueError): self.vizier._check_coords((np.nan, 0.))
