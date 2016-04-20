91690bd34048ce6cf6006817e2de8da164767053
nylee23 <nylee23@gmail.com>
2016-04-20 11:58:58 +0200

    Began cross-match module, added a target source that has a match in test_cat.csv'

--- cross_match.py
+++ cross_match.py
@@ -16,6 +16,7 @@
 # from astropy.table import Table
 from astropy.coordinates import SkyCoord
 from astropy import units as u
+import darkphot_local_catalog as dp_local
 
 # Set up loggers
 import logging
@@ -36,16 +37,38 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 # logger.addHandler(file_handler)
 
-def cross_match(pos, full_table, name_ra_col = 'RA', name_dec_col='Dec'):
-    """ """
-    ra_input, dec_input = zip(*pos)
+
+def cross_match(desired_pos, full_table, name_ra_col = 'RA', name_dec_col='Dec', match_radius=1.5):
+    """
+    Keywords:
+        match_radius - matching radius in arcsec
+    """
+    ra_input, dec_input = zip(*desired_pos)
     coord_input = SkyCoord(ra=ra_input*u.degree, dec=dec_input*u.degree)
 
     ra_catalog = np.array(full_table[name_ra_col])
-    dec_catalog = np.array(full_table[name_dec_col])e
+    dec_catalog = np.array(full_table[name_dec_col])
     coord_catalog = SkyCoord(ra=ra_catalog*u.degree, dec=dec_catalog*u.degree)
 
     idx, d2d, d3d = coord_input.match_to_catalog_sky(coord_catalog)
 
     return idx
+
+
+
+#####################
+###### Run Code #####
+#####################
+if __name__ == '__main__':
+
+    local_cat = dp_local.LocalCatalog()
+    ## A couple sources, including one that doesn't have any counterparts
+    fname_catalog = 'example_data/test_cat.csv'
+    fname_param = 'example_data/input_local_cat.param'
+    fname_object_names_sel = 'example_data/input_names.txt'
+    local_cat.add_local_catalog(fname_catalog, fname_param, fname_object_names_sel, name_id_col='NAME')
+    full_catalog = local_cat._read_text_catalog('csv')
+    target_pos = local_cat.object_sel_coordinates
+
+
 
